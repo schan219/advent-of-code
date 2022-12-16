1fa82dc2bf11281d48f1bdb25dd7ca5c22284a28
Stephanie Chan <stephaniechan219@gmail.com>
2022-12-15 16:24:26 -0800

    Add second puzzle and fix first puzzle

--- 2022/day1/main.py
+++ 2022/day1/main.py
@@ -24,11 +24,68 @@ def most_calories(calories_list):
             continue
         running_max += int(line)
 
+    if running_max > 0 and running_max > max_calories:
+        max_calories = running_max
+
     return max_calories
 
+def top_three_most_calories(calories_list):
+    """
+        Finds the sum of the calories carried by the elf carrying the most calories.
+
+        Parameters
+        ----------
+            calories_list : list[str]
+                list of parsed calories
+    """
+
+    first_max_calories = 0
+    second_max_calories = 0
+    third_max_calories = 0
+    running_max = 0
+
+    for line in calories_list:
+        if not line:
+            # code to determine which max calories variable to store in.
+            arr = [third_max_calories, second_max_calories, first_max_calories]
+            arr = sorted(arr, reverse=True)
+
+            first_max_calories = arr[0]
+            second_max_calories = arr[1]
+            third_max_calories = arr[2]
+
+            if running_max > arr[2]:
+                third_max_calories = running_max
+
+            running_max = 0
+
+            continue
+
+        running_max += int(line)
+
+    if running_max > 0:
+            arr = [third_max_calories, second_max_calories, first_max_calories]
+            arr = sorted(arr, reverse=True)
+
+            first_max_calories = arr[0]
+            second_max_calories = arr[1]
+            third_max_calories = arr[2]
+
+            if running_max > arr[2]:
+                third_max_calories = running_max
+
+
+    return first_max_calories + second_max_calories + third_max_calories
+
 if __name__ == '__main__':
-    with open('input.txt', encoding='UTF-8') as file:
+    TEST = 'example.txt'
+    REAL = 'input.txt'
+
+    with open(TEST, encoding='UTF-8') as file:
         lines = [line.rstrip() for line in file]
 
         RESULT = most_calories(lines)
+        print(RESULT)
+
+        RESULT = top_three_most_calories(lines)
         print(RESULT)
